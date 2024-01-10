3ac5ae9dead618e169a9d8b0066362430d99e80d
Sven Eberth <mail@sveneberth.de>
2024-01-10 20:54:30 +0100

    Export logical model to SQL

--- model/export/Skeletons.sql
+++ model/export/Skeletons.sql
@@ -62,7 +62,7 @@ CREATE TABLE ShippingPreConditionRelSkel (
 CREATE TABLE ShippingSkel (
  key VARCHAR(64) NOT NULL,
  name VARCHAR(10) NOT NULL,
- Kundenkommentar TEXT,
+ description TEXT,
  shipping_cost FLOAT(10),
  supplier VARCHAR(255) NOT NULL,
  delivery_time_min INT NOT NULL,
@@ -92,15 +92,15 @@ CREATE TABLE AbstractArticleSkel (
  key VARCHAR(64) NOT NULL,
  shop_name VARCHAR(255),
  shop_description TEXT,
- Verkaufspreis FLOAT(10),
- UVP FLOAT(10),
+ shop_price_retail FLOAT(10),
+ shop_price_recommended FLOAT(10),
  shop_availability VARCHAR(255),
  shop_listed BIT(1),
- Produktbild VARCHAR(255),
- Artikelnummer VARCHAR(10),
- Steuersatz VARCHAR(64) NOT NULL,
- Versandkosten VARCHAR(64),
- Elektro G BIT(1),
+ shop_image VARCHAR(255),
+ shop_art_no_or_gtin VARCHAR(10),
+ shop_vat VARCHAR(64) NOT NULL,
+ shop_shipping VARCHAR(64),
+ shop_is_weee BIT(1),
  shop_is_low_price BIT(1) NOT NULL
 );
 
@@ -193,9 +193,9 @@ ALTER TABLE ShippingConfigSkel ADD CONSTRAINT FK_ShippingConfigSkel_0 FOREIGN KE
 ALTER TABLE UserSkel ADD CONSTRAINT FK_UserSkel_0 FOREIGN KEY (wishlist) REFERENCES CartNodeSkel (key);
 
 
-ALTER TABLE AbstractArticleSkel ADD CONSTRAINT FK_AbstractArticleSkel_0 FOREIGN KEY (Produktbild) REFERENCES File (key);
-ALTER TABLE AbstractArticleSkel ADD CONSTRAINT FK_AbstractArticleSkel_1 FOREIGN KEY (Steuersatz) REFERENCES VatSkel (key);
-ALTER TABLE AbstractArticleSkel ADD CONSTRAINT FK_AbstractArticleSkel_2 FOREIGN KEY (Versandkosten) REFERENCES ShippingConfigSkel (key);
+ALTER TABLE AbstractArticleSkel ADD CONSTRAINT FK_AbstractArticleSkel_0 FOREIGN KEY (shop_image) REFERENCES File (key);
+ALTER TABLE AbstractArticleSkel ADD CONSTRAINT FK_AbstractArticleSkel_1 FOREIGN KEY (shop_vat) REFERENCES VatSkel (key);
+ALTER TABLE AbstractArticleSkel ADD CONSTRAINT FK_AbstractArticleSkel_2 FOREIGN KEY (shop_shipping) REFERENCES ShippingConfigSkel (key);
 
 
 ALTER TABLE CartItemSkel ADD CONSTRAINT FK_CartItemSkel_0 FOREIGN KEY (parententry) REFERENCES CartNodeSkel (key);
@@ -214,3 +214,119 @@ ALTER TABLE OrderSkel ADD CONSTRAINT FK_OrderSkel_1 FOREIGN KEY (customer) REFER
 ALTER TABLE OrderSkel ADD CONSTRAINT FK_OrderSkel_2 FOREIGN KEY (cart) REFERENCES CartNodeSkel (key);
 
 
+COMMENT ON TABLE AddressSkel IS 'AddressSkel';
+COMMENT ON COLUMN AddressSkel.key IS '';
+COMMENT ON COLUMN AddressSkel.customer_type IS '';
+COMMENT ON COLUMN AddressSkel.salutation IS '';
+COMMENT ON COLUMN AddressSkel.company_name IS '';
+COMMENT ON COLUMN AddressSkel.firstname IS '';
+COMMENT ON COLUMN AddressSkel.lastname IS '';
+COMMENT ON COLUMN AddressSkel.street_name IS '';
+COMMENT ON COLUMN AddressSkel.street_number IS '';
+COMMENT ON COLUMN AddressSkel.address_addition IS '';
+COMMENT ON COLUMN AddressSkel.zip_code IS '';
+COMMENT ON COLUMN AddressSkel.city IS '';
+COMMENT ON COLUMN AddressSkel.country IS '';
+COMMENT ON COLUMN AddressSkel.customer IS '';
+COMMENT ON COLUMN AddressSkel.is_default IS '';
+COMMENT ON COLUMN AddressSkel.address_type IS '';
+COMMENT ON TABLE CartNodeSkel IS 'CartNodeSkel';
+COMMENT ON COLUMN CartNodeSkel.key IS '';
+COMMENT ON COLUMN CartNodeSkel.total IS '';
+COMMENT ON COLUMN CartNodeSkel.vat_value IS '';
+COMMENT ON COLUMN CartNodeSkel.vat_rate IS '';
+COMMENT ON COLUMN CartNodeSkel.shipping_address IS '';
+COMMENT ON COLUMN CartNodeSkel.customer_comment IS '';
+COMMENT ON COLUMN CartNodeSkel.name IS '';
+COMMENT ON COLUMN CartNodeSkel.parententry IS '';
+COMMENT ON COLUMN CartNodeSkel.cart_type IS '';
+COMMENT ON COLUMN CartNodeSkel.shipping IS '';
+COMMENT ON COLUMN CartNodeSkel.discount IS '';
+COMMENT ON TABLE File IS '';
+COMMENT ON COLUMN File.key IS '';
+COMMENT ON TABLE ShippingConfigSkel IS 'ShippingConfigSkel';
+COMMENT ON COLUMN ShippingConfigSkel.key IS '';
+COMMENT ON COLUMN ShippingConfigSkel.name IS '';
+COMMENT ON COLUMN ShippingConfigSkel.shipping_skel IS '';
+COMMENT ON TABLE ShippingPreConditionRelSkel IS '';
+COMMENT ON COLUMN ShippingPreConditionRelSkel.minimum_order_value IS '';
+COMMENT ON COLUMN ShippingPreConditionRelSkel.country IS '';
+COMMENT ON COLUMN ShippingPreConditionRelSkel.zip_code IS '';
+COMMENT ON TABLE ShippingSkel IS 'ShippingSkel';
+COMMENT ON COLUMN ShippingSkel.key IS '';
+COMMENT ON COLUMN ShippingSkel.name IS '';
+COMMENT ON COLUMN ShippingSkel.description IS 'Kundenkommentar';
+COMMENT ON COLUMN ShippingSkel.shipping_cost IS '';
+COMMENT ON COLUMN ShippingSkel.supplier IS '';
+COMMENT ON COLUMN ShippingSkel.delivery_time_min IS '';
+COMMENT ON COLUMN ShippingSkel.delivery_time_max IS '';
+COMMENT ON TABLE UserSkel IS 'UserSkel';
+COMMENT ON COLUMN UserSkel.key IS '';
+COMMENT ON COLUMN UserSkel.wishlist IS '';
+COMMENT ON TABLE VatSkel IS 'VatSkel';
+COMMENT ON COLUMN VatSkel.key IS '';
+COMMENT ON COLUMN VatSkel.rate IS '';
+COMMENT ON TABLE AbstractArticleSkel IS 'AbstractArticleSkel';
+COMMENT ON COLUMN AbstractArticleSkel.key IS '';
+COMMENT ON COLUMN AbstractArticleSkel.shop_name IS 'shop_name';
+COMMENT ON COLUMN AbstractArticleSkel.shop_description IS 'shop_description';
+COMMENT ON COLUMN AbstractArticleSkel.shop_price_retail IS 'Verkaufspreis';
+COMMENT ON COLUMN AbstractArticleSkel.shop_price_recommended IS 'UVP';
+COMMENT ON COLUMN AbstractArticleSkel.shop_availability IS '';
+COMMENT ON COLUMN AbstractArticleSkel.shop_listed IS 'shop_listed';
+COMMENT ON COLUMN AbstractArticleSkel.shop_image IS 'Produktbild';
+COMMENT ON COLUMN AbstractArticleSkel.shop_art_no_or_gtin IS 'Artikelnummer';
+COMMENT ON COLUMN AbstractArticleSkel.shop_vat IS 'Steuersatz';
+COMMENT ON COLUMN AbstractArticleSkel.shop_shipping IS 'Versandkosten';
+COMMENT ON COLUMN AbstractArticleSkel.shop_is_weee IS 'Elektro G';
+COMMENT ON COLUMN AbstractArticleSkel.shop_is_low_price IS '';
+COMMENT ON TABLE CartItemSkel IS 'CartItemSkel';
+COMMENT ON COLUMN CartItemSkel.key IS '';
+COMMENT ON COLUMN CartItemSkel.parententry IS '';
+COMMENT ON COLUMN CartItemSkel.article IS '';
+COMMENT ON COLUMN CartItemSkel.**article_data_skey IS '';
+COMMENT ON COLUMN CartItemSkel.project_data IS '';
+COMMENT ON TABLE DiscountConditionSkel IS '';
+COMMENT ON COLUMN DiscountConditionSkel.key IS '';
+COMMENT ON COLUMN DiscountConditionSkel.code_type IS '';
+COMMENT ON COLUMN DiscountConditionSkel.application_domain IS '';
+COMMENT ON COLUMN DiscountConditionSkel.quantity_volume IS '';
+COMMENT ON COLUMN DiscountConditionSkel.quantity_used IS '';
+COMMENT ON COLUMN DiscountConditionSkel.individual_codes_amount IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_code IS '';
+COMMENT ON COLUMN DiscountConditionSkel.individual_codes_prefix IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_minimum_order_value IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_date_start IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_date_end IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_language IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_country IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_minimum_quantity IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_customer_group IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_combinable_other_discount IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_combinable_low_price IS '';
+COMMENT ON COLUMN DiscountConditionSkel.scope_article IS '';
+COMMENT ON COLUMN DiscountConditionSkel.is_subcode IS '';
+COMMENT ON COLUMN DiscountConditionSkel.parent_code IS '';
+COMMENT ON TABLE DiscountSkel IS '';
+COMMENT ON COLUMN DiscountSkel.key IS '';
+COMMENT ON COLUMN DiscountSkel.name IS '';
+COMMENT ON COLUMN DiscountSkel.description IS '';
+COMMENT ON COLUMN DiscountSkel.discount_type IS '';
+COMMENT ON COLUMN DiscountSkel.absolute IS '';
+COMMENT ON COLUMN DiscountSkel.percentage IS '';
+COMMENT ON COLUMN DiscountSkel.condition IS '';
+COMMENT ON COLUMN DiscountSkel.condition_operator IS '';
+COMMENT ON TABLE OrderSkel IS 'OrderSkel';
+COMMENT ON COLUMN OrderSkel.key IS '';
+COMMENT ON COLUMN OrderSkel.billing_address IS '';
+COMMENT ON COLUMN OrderSkel.customer IS '';
+COMMENT ON COLUMN OrderSkel.cart IS '';
+COMMENT ON COLUMN OrderSkel.total IS '';
+COMMENT ON COLUMN OrderSkel.order_uid IS '';
+COMMENT ON COLUMN OrderSkel.payment_provider IS '';
+COMMENT ON COLUMN OrderSkel.is_ordered IS '';
+COMMENT ON COLUMN OrderSkel.is_paid IS '';
+COMMENT ON COLUMN OrderSkel.is_rts IS '';
+COMMENT ON COLUMN OrderSkel.state IS '';
+COMMENT ON COLUMN OrderSkel.email IS '';
+COMMENT ON COLUMN OrderSkel.project_data IS '';
