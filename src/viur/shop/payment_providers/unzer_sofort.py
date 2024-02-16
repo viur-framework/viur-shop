5522038472e2135639443d052f588fbd0326629c
Sven Eberth <se@mausbrand.de>
2024-02-16 15:51:40 +0100

    Implement sofort payment type

---
+++ src/viur/shop/payment_providers/unzer_sofort.py
@@ -0,0 +1,36 @@
+import logging
+import typing as t
+
+import unzer
+from unzer.model import PaymentType
+from viur.core import errors, exposed
+from viur.core.skeleton import SkeletonInstance
+
+from .unzer_abstract import UnzerAbstract
+
+logger = logging.getLogger("viur.shop").getChild(__name__)
+
+
+class UnzerSofort(UnzerAbstract):
+    name = "unzer-sofort"
+
+    def get_payment_type(
+        self,
+        order_skel: SkeletonInstance,
+    ) -> PaymentType:
+        type_id = order_skel["payment"]["payments"][-1]["type_id"]
+        return unzer.Sofort(key=type_id)
+
+    def charge(
+        self,
+        order_skel: SkeletonInstance,
+    ) -> t.Any:
+        raise errors.NotImplemented()
+
+    @exposed
+    def webhook(self):
+        raise errors.NotImplemented()
+
+    @exposed
+    def get_debug_information(self):
+        raise errors.NotImplemented()
