1c9614db4c016374f7204e9cb33780b689dc2d28
Sven Eberth <mail@sveneberth.de>
2024-03-05 14:49:00 +0100

    feat: Add more translations (related to #6)

--- src/viur/shop/data/translations.py
+++ src/viur/shop/data/translations.py
@@ -5,10 +5,40 @@ TRANSLATIONS = {
         # "_default_text": "",
         "_hint": "modulename: address",
     },
+    "viur.shop.module.api": {
+        "en": "api",
+        "de": "API",
+        "_hint": "modulename: api",
+    },
     "viur.shop.module.cart": {
         "en": "cart",
         "de": "Warenkorb",
         "_hint": "modulename: cart",
+    },
+    "viur.shop.module.discount": {
+        "en": "discount",
+        "de": "Rabatte",
+        "_hint": "modulename: discount",
+    },
+    "viur.shop.module.discount_condition": {
+        "en": "discount conditions",
+        "de": "Rabatt Bedingungen",
+        "_hint": "modulename: discount_condition",
+    },
+    "viur.shop.module.order": {
+        "en": "order",
+        "de": "Bestellungen",
+        "_hint": "modulename: order",
+    },
+    "viur.shop.module.shipping": {
+        "en": "shipping",
+        "de": "Versand",
+        "_hint": "modulename: shipping",
+    },
+    "viur.shop.module.shipping_config": {
+        "en": "shipping_config",
+        "de": "Versandkonfiguration",
+        "_hint": "modulename: shipping_config",
     },
     "viur.shop.module.vat": {
         "en": "vat",
@@ -16,4 +46,4 @@ TRANSLATIONS = {
         "_hint": "modulename: vat",
     },
 }
-"""Initial translaions used inside the shop"""
+"""Initial translations used inside the shop"""
