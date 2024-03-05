eee76d4efc7a6c50c1eee87e12e654a327b11a5f
Sven Eberth <mail@sveneberth.de>
2024-03-05 14:34:30 +0100

    feat: Setup translations for viur-shop (part 2)

---
+++ src/viur/shop/data/translations.py
@@ -0,0 +1,19 @@
+TRANSLATIONS = {
+    "viur.shop.module.address": {
+        "en": "addresses",
+        "de": "Adressen",
+        # "_default_text": "",
+        "_hint": "modulename: address",
+    },
+    "viur.shop.module.cart": {
+        "en": "cart",
+        "de": "Warenkorb",
+        "_hint": "modulename: cart",
+    },
+    "viur.shop.module.vat": {
+        "en": "vat",
+        "de": "Steuersätze",
+        "_hint": "modulename: vat",
+    },
+}
+"""Initial translaions used inside the shop"""
