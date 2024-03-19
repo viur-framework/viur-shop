5ed38b283c408214a33039db8f474cd6df866081
phneutral <chris@ph-neutral.net>
2024-03-19 12:57:03 +0100

    feat: Change and add translations

--- src/viur/shop/data/translations.py
+++ src/viur/shop/data/translations.py
@@ -1,55 +1,87 @@
 TRANSLATIONS = {
     # Module names
     "viur.shop.module.address": {
-        "en": "addresses",
+        "en": "Addresses",
         "de": "Adressen",
         # "_default_text": "",
         "_hint": "modulename: address",
     },
     "viur.shop.module.api": {
-        "en": "api",
+        "en": "API",
         "de": "API",
         "_hint": "modulename: api",
     },
     "viur.shop.module.cart": {
-        "en": "cart",
-        "de": "Warenkorb",
+        "en": "Carts",
+        "de": "Warenkörbe",
         "_hint": "modulename: cart",
     },
     "viur.shop.module.discount": {
-        "en": "discount",
+        "en": "Discounts",
         "de": "Rabatte",
         "_hint": "modulename: discount",
     },
     "viur.shop.module.discount_condition": {
-        "en": "discount conditions",
-        "de": "Rabatt Bedingungen",
+        "en": "Discount conditions",
+        "de": "Rabattbedingungen",
         "_hint": "modulename: discount_condition",
     },
     "viur.shop.module.order": {
-        "en": "order",
+        "en": "Orders",
         "de": "Bestellungen",
         "_hint": "modulename: order",
     },
     "viur.shop.module.shipping": {
-        "en": "shipping",
+        "en": "Shipping",
         "de": "Versand",
         "_hint": "modulename: shipping",
     },
     "viur.shop.module.shipping_config": {
-        "en": "shipping_config",
+        "en": "Shipping configuration",
         "de": "Versandkonfiguration",
         "_hint": "modulename: shipping_config",
     },
     "viur.shop.module.vat": {
-        "en": "vat",
+        "en": "Tax rates",
         "de": "Steuersätze",
         "_hint": "modulename: vat",
     },
-    # Payment Providers
+    # Payment providers
     "viur.shop.payment_provider.unzer-paypal": {
         "_default_text": "PayPal via Unzer",
         "_hint": "viur-shop payment provider: unzer-paypal",
+    },
+    # Enums
+    # Availability
+    "instock": {
+        "en": "Instock",
+        "fr": "En stock",
+        "de": "Lieferbar",
+        "_hint": "availabilty: instock",
+    },
+    "outofstock": {
+        "en": "Out of stock",
+        "fr": "Non disponible actuellement",
+        "de": "Zur Zeit nicht lieferbar",
+        "_hint": "availabilty: outofstock",
+    },
+    "limited": {
+        "en": "Limited",
+        "fr": "Limité",
+        "de": "Lieferung verzögert",
+        "_hint": "availabilty: limited",
+    },
+    "discontinued": {
+        "en": "Discontinued",
+        "fr": "Plus disponible",
+        "de": "Nicht mehr verfügbar",
+        "_hint": "availabilty: discontinued",
+    },
+    "preorder": {
+        "en": "Preorder",
+        "fr": "Bientôt disponible à nouveau",
+        "de": "Vorbestellung möglich",
+        "_hint": "availabilty: preorder",
     },
 }
 """Initial translations used inside the shop"""
