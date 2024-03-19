9aeb5536a9e9fbd348c359a7f4f3520b909d5f0a
Sven Eberth <mail@sveneberth.de>
2024-03-19 14:01:54 +0100

    Prepare translations dict

--- src/viur/shop/skeletons/__init__.py
+++ src/viur/shop/skeletons/__init__.py
@@ -25,6 +25,9 @@ from .shipping_config import ShippingConfigSkel
 from .shipping_precondition import ShippingPreconditionRelSkel
 from .vat import VatSkel
 
+# tmp used to generate translation dict
+_tr = {}
+
 # Set translated description of the bones using a schema with the bone name
 for _key, _value in locals().copy().items():
     if isinstance(_value, type) and issubclass(_value, Skeleton) and _value.__module__.startswith("viur.shop"):
@@ -35,3 +38,11 @@ for _key, _value in locals().copy().items():
                     _bone_name,
                     f"bone {_bone_name}<{type(_bone_instance).__name__}> in {_value.__name__} in viur.shop"
                 )
+                _tr[_bone_instance.descr.key] = {
+                    "en": _bone_name.replace("_", " ").capitalize(),
+                    "fr": "",
+                    "de": "",
+                    "_hint": _bone_instance.descr.hint,
+                }
+
+# pprint(tr)
