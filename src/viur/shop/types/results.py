c89f839939333f3b8affe6cdb8a96c9197840fcd
Sven Eberth <se@mausbrand.de>
2024-11-22 21:11:44 +0100

    feat: Implement more Order API, checks and types

---
+++ src/viur/shop/types/results.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import typing as t
+
+from .data import ClientError
+from ..globals import SHOP_LOGGER
+
+if t.TYPE_CHECKING:
+    from viur.shop import OrderSkel, SkeletonInstance_T
+
+logger = SHOP_LOGGER.getChild(__name__)
+
+
+class StatusError(t.TypedDict):
+    status: bool
+    errors: list[ClientError]
+
+
+class OrderViewResult(t.TypedDict):
+    skel: SkeletonInstance_T[OrderSkel]
+    can_checkout: StatusError
+    can_order: StatusError
