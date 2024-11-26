f0bcb66d213bbf2b1d25c50fbb3a8e3c2207437b
Sven Eberth <mail@sveneberth.de>
2024-11-26 16:22:00 +0100

    feat: Implement more Unzer payment providers

---
+++ src/viur/shop/payment_providers/unzer_bancontact.py
@@ -0,0 +1,19 @@
+import unzer
+from unzer.model import PaymentType
+
+from viur.core.skeleton import SkeletonInstance
+from .unzer_abstract import UnzerAbstract
+from ..globals import SHOP_LOGGER
+
+logger = SHOP_LOGGER.getChild(__name__)
+
+
+class UnzerBancontact(UnzerAbstract):
+    name = "unzer-bancontact"
+
+    def get_payment_type(
+        self,
+        order_skel: SkeletonInstance,
+    ) -> PaymentType:
+        type_id = order_skel["payment"]["payments"][-1]["type_id"]
+        return unzer.Bancontact(key=type_id)
