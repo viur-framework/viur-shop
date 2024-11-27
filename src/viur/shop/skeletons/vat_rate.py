fe9379cc391081a074dc2d5df93036749c8b940e
Sven Eberth <se@mausbrand.de>
2024-11-27 18:56:17 +0100

    feat: Improve vat rate

--- src/viur/shop/skeletons/vat_rate.py
+++ src/viur/shop/skeletons/vat_rate.py
@@ -2,21 +2,21 @@ import typing as t  # noqa
 
 from viur.core.bones import *
 from viur.core.skeleton import Skeleton
-
 from .vat import VatSkel
 from ..globals import SHOP_LOGGER
 
 logger = SHOP_LOGGER.getChild(__name__)
 
 
-class VatRateSkel(Skeleton):  # STATE: Complete (as in model)
+class VatRateSkel(Skeleton):
     kindName = "{{viur_shop_modulename}}_vat_rate"
-
-    # TODO: add descr bone?!
 
     name = StringBone(
         compute=Compute(
-            lambda skel: f'{skel["country"]} | {", ".join(": ".join(map(str, (v["rate"], v["value"]))) for v in skel["configuration"])}',
+            lambda skel: ' | '.join((
+                f'{skel["country"]}',
+                ", ".join(f'{v["rate"]}: {v["value"]}' for v in skel["configuration"]),
+            )),
             ComputeInterval(ComputeMethod.OnWrite)
         ),
     )
