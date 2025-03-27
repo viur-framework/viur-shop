viur-framework/viur-shop

commit abf47af6fa840324b15b2eb42bde8c91f2155161

chore: Enforce `viur-core >= 3.7` (#102)
This allows to use only the newer `.read()` and `.write()` syntax and newer features

src/viur/shop/modules/cart.py
@@ -41,9 +41,9 @@ class Cart(ShopModuleAbstract, Tree):
         if sub_skel is None:
             return cls()  # noqa
         if isinstance(sub_skel, list):
-            return cls.subSkel(*sub_skel)
+            return cls.subskel(*sub_skel)
         else:
-            return cls.subSkel(sub_skel)
+            return cls.subskel(sub_skel)
 
     def canView(self, skelType: SkelType, skel: SkeletonInstance) -> bool:
         if super().canView(skelType, skel):
@@ -61,7 +61,7 @@ class Cart(ShopModuleAbstract, Tree):
     def current_session_cart_key(self):
         if user := current.user.get():
             user_skel = conf.main_app.vi.user.viewSkel()
-            user_skel.fromDB(user["key"])
+            user_skel.read(user["key"])
             if user_skel["basket"]:
                 self.session["session_cart_key"] = user_skel["basket"]["dest"]["key"]
                 current.session.get().markChanged()
@@ -71,7 +71,7 @@ class Cart(ShopModuleAbstract, Tree):
     @property
     def current_session_cart(self):  # TODO: Caching
         skel = self.viewSkel("node")
-        if not skel.fromDB(self.current_session_cart_key):
+        if not skel.read(self.current_session_cart_key):
             logger.critical(f"Invalid session_cart_key {self.current_session_cart_key} ?! Not in DB!")
             self.detach_session_cart()
             return self.current_session_cart
@@ -85,8 +85,8 @@ class Cart(ShopModuleAbstract, Tree):
             root_node["is_root_node"] = True
             root_node["name"] = f"Session Cart of {user} created at {utils.utcNow()}"
             root_node["cart_type"] = CartType.BASKET
-            key = root_node.toDB()
-            self.session["session_cart_key"] = key
+            root_node.write()
+            self.session["session_cart_key"] = root_node["key"]
             current.session.get().markChanged()
             # Store basket at the user skel, it will be shared over multiple sessions / devices
             if user := current.user.get():
@@ -104,9 +104,9 @@ class Cart(ShopModuleAbstract, Tree):
     @staticmethod
     def _set_basket_txn(user_key: db.Key, basket_key: db.Key | None) -> SkeletonInstance:
         user_skel = conf.main_app.vi.user.editSkel()
-        user_skel.fromDB(user_key)
+        user_skel.read(user_key)
         user_skel.setBoneValue("basket", basket_key)
-        user_skel.toDB()
+        user_skel.write()
         return user_skel
 
     def get_available_root_nodes(self, *args, **kwargs) -> list[dict[t.Literal["name", "key"], str]]:
@@ -156,7 +156,7 @@ class Cart(ShopModuleAbstract, Tree):
         """
         # TODO: return (okay_status, reason, skel) tuple/Dataclass?
         skel = self.viewSkel("node")
-        if not skel.fromDB(node_key):
+        if not skel.read(node_key):
             logger.debug(f"fail reason: 404")
             return False
         logger.debug(f'{skel=}')
@@ -220,7 +220,7 @@ class Cart(ShopModuleAbstract, Tree):
         if not isinstance(cart_key, db.Key):
             raise TypeError(f"cart_key must be an instance of db.Key")
         skel = self.viewSkel(skel_type)
-        if not skel.fromDB(cart_key):
+        if not skel.read(cart_key):
             logger.debug(f"Cart {cart_key} does not exist")
             return None
         if not self.canView(skel_type, skel):
@@ -274,13 +274,13 @@ class Cart(ShopModuleAbstract, Tree):
             res = skel.setBoneValue("article", article_key)
             skel["parententry"] = parent_cart_key
             parent_skel = self.viewSkel("node")
-            assert parent_skel.fromDB(parent_cart_key)
+            assert parent_skel.read(parent_cart_key)
             if parent_skel["is_root_node"]:
                 skel["parentrepo"] = parent_skel["key"]
             else:
                 skel["parentrepo"] = parent_skel["parentrepo"]
             article_skel: SkeletonInstance = self.shop.article_skel()
-            if not article_skel.fromDB(article_key):
+            if not article_skel.read(article_key):
                 raise errors.NotFound(f"Article with key {article_key=} does not exist!")
             if not article_skel["shop_listed"]:
                 # logger.debug(f"not listed: {article_skel=}")
@@ -333,7 +333,7 @@ class Cart(ShopModuleAbstract, Tree):
                 descr_appendix=f'Quantity of free article cannot be greater than 1! (reached {skel["quantity"]})'
             )
         skel = self.additional_add_or_update_article(skel, **kwargs)
-        key = skel.toDB()
+        skel.write()
         EVENT_SERVICE.call(Event.ARTICLE_CHANGED, skel=skel, deleted=False)
         self.clear_children_cache()
         # TODO: Validate quantity with hook (stock availability)
@@ -342,7 +342,7 @@ class Cart(ShopModuleAbstract, Tree):
                 skel=parent_skel
             )
             EVENT_SERVICE.call(Event.CART_CHANGED, skel=parent_skel, deleted=False)
-            parent_skel.toDB()
+            parent_skel.write()
 
         return skel
 
@@ -366,7 +366,7 @@ class Cart(ShopModuleAbstract, Tree):
         parent_skel = self.viewSkel("node")
         if not self.is_valid_node(new_parent_cart_key):
             raise e.InvalidArgumentException("parent_cart_key", parent_cart_key)
-        if not parent_skel.fromDB(new_parent_cart_key):
+        if not parent_skel.read(new_parent_cart_key):
             raise e.InvalidArgumentException(
                 "new_parent_cart_key", new_parent_cart_key,
                 f"Target cart node does not exist"
@@ -377,7 +377,7 @@ class Cart(ShopModuleAbstract, Tree):
                 f"Target cart node is inside a different repo"
             )
         skel["parententry"] = new_parent_cart_key
-        skel.toDB()
+        skel.write()
         EVENT_SERVICE.call(Event.ARTICLE_CHANGED, skel=skel, deleted=False)
         return skel
 
@@ -411,7 +411,7 @@ class Cart(ShopModuleAbstract, Tree):
             discount_key=discount_key,
         )
         skel = self.additional_cart_add(skel, **kwargs)
-        skel.toDB()
+        skel.write()
         EVENT_SERVICE.call(Event.CART_CHANGED, skel=skel, deleted=False)
         self.onAdded("node", skel)
         return skel
@@ -441,7 +441,7 @@ class Cart(ShopModuleAbstract, Tree):
         # TODO: must be inside a own root node ...
         # if not self.canEdit(skel):
         #     raise errors.Forbidden
-        assert skel.fromDB(cart_key)
+        assert skel.read(cart_key)
         skel = self._cart_set_values(
             skel=skel,
             parent_cart_key=parent_cart_key,
@@ -452,7 +452,7 @@ class Cart(ShopModuleAbstract, Tree):
             discount_key=discount_key,
         )
         self.additional_cart_update(skel, **kwargs)
-        skel.toDB()
+        skel.write()
         EVENT_SERVICE.call(Event.CART_CHANGED, skel=skel, deleted=False)
         return skel
 
@@ -478,7 +478,7 @@ class Cart(ShopModuleAbstract, Tree):
                 if not self.is_valid_node(parent_cart_key):
                     raise e.InvalidArgumentException("parent_cart_key", parent_cart_key)
                 parent_skel = self.viewSkel("node")
-                assert parent_skel.fromDB(parent_cart_key)
+                assert parent_skel.read(parent_cart_key)
                 if parent_skel["is_root_node"]:
                     skel["parentrepo"] = parent_skel["key"]
                 else:
@@ -529,7 +529,7 @@ class Cart(ShopModuleAbstract, Tree):
     ) -> None:
         self.deleteRecursive(cart_key)
         skel = self.editSkel("node")
-        if not skel.fromDB(cart_key):
+        if not skel.read(cart_key):
             raise errors.NotFound
         if skel["parententry"] is None or skel["is_root_node"]:
             logger.info(f"{skel['key']} was a root node!")
@@ -625,14 +625,14 @@ class Cart(ShopModuleAbstract, Tree):
         #   ensure each article still exists and shop_listed is True
         return NotImplemented
         cart_skel = self.editSkel("node")
-        assert cart_skel.fromDB(cart_key)
+        assert cart_skel.read(cart_key)
         """
         skel["shop_vat_rate_value"] = self.shop.vat_rate.get_vat_rate_for_country(
             country=order_skel["billing_address"]["dest"]["country"],
             category=article_skel["shop_vat_rate_category"],
         )
         """
-        cart_skel.toDB()
+        cart_skel.write()
 
     # -------------------------------------------------------------------------
 
@@ -642,13 +642,13 @@ class Cart(ShopModuleAbstract, Tree):
     ) -> list[SkeletonInstance]:
         if isinstance(leaf_key_or_skel, db.Key):
             skel = self.viewSkel("leaf")
-            skel.fromDB(leaf_key_or_skel)
+            skel.read(leaf_key_or_skel)
         else:
             skel = leaf_key_or_skel
         discounts = []
         while (pk := skel["parententry"]):
             skel = self.viewSkel("node", sub_skel="discount")
-            if not skel.fromDB(pk):
+            if not skel.read(pk):
                 raise InvalidStateError(f"{pk=} doesn't exist!")
             if discount := skel["discount"]:
                 discounts.append(discount["dest"])
@@ -661,10 +661,10 @@ class Cart(ShopModuleAbstract, Tree):
         for key, value in kwargs.items():
             new_parent_skel[key] = value  # TODO: use .setBoneValue?
         self.onAdd("node", new_parent_skel)
-        new_parent_skel.toDB()
+        new_parent_skel.write()
         self.onAdded("node", new_parent_skel)
         EVENT_SERVICE.call(Event.CART_CHANGED, skel=new_parent_skel, deleted=False)
         leaf_skel["parententry"] = new_parent_skel["key"]
-        leaf_skel.toDB()
+        leaf_skel.write()
         EVENT_SERVICE.call(Event.ARTICLE_CHANGED, skel=leaf_skel, deleted=False)
         return new_parent_skel

src/viur/shop/skeletons/cart.py
@@ -1,12 +1,11 @@
 import collections
 import typing as t  # noqa
 
+from viur import toolkit
 from viur.core import db
 from viur.core.bones import *
 from viur.core.prototypes.tree import TreeSkel
 from viur.core.skeleton import SkeletonInstance
-
-from viur import toolkit
 from viur.shop.types import *
 from .vat import VatIncludedSkel
 from ..globals import SHOP_INSTANCE, SHOP_LOGGER
@@ -313,7 +312,7 @@ class CartItemSkel(TreeSkel):  # STATE: Complete (as in model)
         # TODO: Cache this property
         # logger.debug(f'Reading article_skel_full {self.article_skel["key"]=}')
         skel = SHOP_INSTANCE.get().article_skel()
-        assert skel.fromDB(self.article_skel["key"])
+        assert skel.read(self.article_skel["key"])
         return skel
 
     @property
@@ -321,7 +320,7 @@ class CartItemSkel(TreeSkel):  # STATE: Complete (as in model)
         if not (pk := self["parententry"]):
             return None
         skel = SHOP_INSTANCE.get().cart.viewSkel("node")
-        assert skel.fromDB(pk)
+        assert skel.read(pk)
         return skel
 
     @property
@@ -341,7 +340,3 @@ class CartItemSkel(TreeSkel):  # STATE: Complete (as in model)
             ComputeInterval(ComputeMethod.Always)),
     )
     shipping.type = JsonBone.type
-
-    @classmethod
-    def toDB(cls, skelValues: SkeletonInstance, update_relations: bool = True, **kwargs) -> db.Key:
-        return super().toDB(skelValues, update_relations, **kwargs)

pyproject.toml
@@ -9,9 +9,10 @@ build-backend = "setuptools.build_meta"
 name = "viur-shop"
 dynamic = ["version"]
 dependencies = [
-    "viur-toolkit>=0.1.0.dev4"
+    "viur-toolkit>=0.2.0",
+    "viur-core>=3.7.0"
 ]
-requires-python = ">=3.10"
+requires-python = ">=3.11"
 authors = [
     { name = "Sven Eberth", email = "se@mausbrand.de" },
 ]
@@ -23,11 +24,10 @@ readme = "README.md"
 license = { file = "LICENSE" }
 keywords = ["viur", "plugin", "backend", "shop"]
 classifiers = [
-    "Development Status :: 1 - Planning",
+    "Development Status :: 2 - Pre-Alpha",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
-    "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
     "Topic :: Software Development :: Libraries :: Python Modules",

src/viur/shop/modules/address.py
@@ -82,7 +82,7 @@ class Address(ShopModuleAbstract, List):
         for other_skel in query.fetch(100):
             if skel["key"] != other_skel["key"]:
                 other_skel["is_default"] = False
-                other_skel.toDB()
+                other_skel.write()
 
 
 Address.json = True

src/viur/shop/modules/discount.py
@@ -51,7 +51,7 @@ class Discount(ShopModuleAbstract, List):
 
         skel = self.viewSkel()
         if discount_key is not None:
-            if not skel.fromDB(discount_key):
+            if not skel.read(discount_key):
                 raise errors.NotFound
             return [skel]
         elif code is not None:
@@ -152,7 +152,7 @@ class Discount(ShopModuleAbstract, List):
                     for leaf_skel in leaf_skels:
                         # Assign discount on new parent node for the leaf where the article is
                         parent_skel = self.shop.cart.viewSkel("node")
-                        assert parent_skel.fromDB(leaf_skel["parententry"])
+                        assert parent_skel.read(leaf_skel["parententry"])
                         if parent_skel["discount"] and parent_skel["discount"]["dest"]["key"] == discount_skel["key"]:
                             logger.info("Parent has already this discount key")
                             continue
@@ -189,7 +189,7 @@ class Discount(ShopModuleAbstract, List):
             cart = None
         else:
             cart = self.shop.cart.viewSkel("node")
-            if not cart.fromDB(cart_key):
+            if not cart.read(cart_key):
                 raise errors.NotFound
 
         if not as_automatically and skel["activate_automatically"]:
@@ -224,7 +224,7 @@ class Discount(ShopModuleAbstract, List):
 
         discount_skel = self.viewSkel()
 
-        if not discount_skel.fromDB(discount_key):
+        if not discount_skel.read(discount_key):
             raise errors.NotFound
         try:
             # Todo what we do when we have more than more condition

src/viur/shop/modules/discount_condition.py
@@ -100,7 +100,7 @@ class DiscountCondition(ShopModuleAbstract, List):
         # logger.debug(pprint.pformat(skel, width=120))
         skel_old = self.viewSkel()
         if skel["key"] is not None:  # not on add
-            skel_old.fromDB(skel["key"])
+            skel_old.read(skel["key"])
         current.request_data.get()[f'shop_skel_{skel["key"]}'] = skel_old
 
     def on_changed(self, skel, event: str):
@@ -119,7 +119,7 @@ class DiscountCondition(ShopModuleAbstract, List):
         """Generate subcodes for a parent individual code."""
         chunk_amount = amount
         while chunk_amount > 0:
-            skel = self.addSkel()  # .subSkel("individual")
+            skel = self.addSkel()  # .subskel("individual")
             skel["is_subcode"] = True
             skel["quantity_volume"] = 1
             skel.setBoneValue("parent_code", parent_key)
@@ -131,7 +131,7 @@ class DiscountCondition(ShopModuleAbstract, List):
 
                 try:
                     self.onAdd(skel)
-                    skel.toDB()
+                    skel.write()
                     self.onAdded(skel)
                     break
                 except ValueError as e:
@@ -164,7 +164,7 @@ class DiscountCondition(ShopModuleAbstract, List):
     def _get_skel(key: db.Key, ttl_hash: int | None = None) -> SkeletonInstance_T["DiscountConditionSkel"] | None:
         # logger.debug(f"_get_skel({key=}, {ttl_hash=})")
         skel = SHOP_INSTANCE.get().discount_condition.viewSkel()
-        if not skel.fromDB(key):
+        if not skel.read(key):
             return None
         return skel  # type: ignore
 
@@ -175,7 +175,7 @@ class DiscountCondition(ShopModuleAbstract, List):
         for cond_skel in query.fetch(100):
             if cond_skel["is_subcode"]:
                 parent_cond_skel = self.viewSkel()
-                assert parent_cond_skel.fromDB(cond_skel["parent_code"]["dest"]["key"])
+                assert parent_cond_skel.read(cond_skel["parent_code"]["dest"]["key"])
                 yield parent_cond_skel
                 # yield cond_skel["parent_code"]["dest"]
             else:
@@ -202,7 +202,7 @@ class DiscountCondition(ShopModuleAbstract, List):
 
         for discount in discounts:
             d_skel = self.shop.discount.viewSkel()
-            d_skel.fromDB(discount)
+            d_skel.read(discount)
             for condition in d_skel["condition"]:
                 # TODO: Increase only "active" conditions in case of OR operator
                 # cond_skel = toolkit.get_full_skel_from_ref_skel(condition["dest"])

src/viur/shop/modules/order.py
@@ -108,7 +108,7 @@ class Order(ShopModuleAbstract, List):
         if not isinstance(order_key, db.Key):
             raise TypeError(f"order_key must be an instance of db.Key")
         skel = self.viewSkel()
-        if not skel.fromDB(order_key):
+        if not skel.read(order_key):
             logger.debug(f"Order {order_key} does not exist")
             return None
         if not self.canView(skel):
@@ -137,7 +137,7 @@ class Order(ShopModuleAbstract, List):
         cart_skel = self.shop.cart.viewSkel("node")
         if not self.shop.cart.is_valid_node(cart_key, root_node=True):
             raise ValueError(f"Invalid {cart_key=}!")
-        assert cart_skel.fromDB(cart_key)
+        assert cart_skel.read(cart_key)
         skel.setBoneValue("cart", cart_key)
         skel["total"] = cart_skel["total"]
         if user := current.user.get():
@@ -162,7 +162,7 @@ class Order(ShopModuleAbstract, List):
             pass
         skel = self.additional_order_add(skel, **kwargs)
         self.onAdd(skel)
-        skel.toDB()
+        skel.write()
         self.current_session_order_key = skel["key"]
         self.onAdded(skel)
         EVENT_SERVICE.call(Event.ORDER_CHANGED, order_skel=skel, deleted=False)
@@ -186,7 +186,7 @@ class Order(ShopModuleAbstract, List):
         if customer_key is not SENTINEL and not isinstance(customer_key, (db.Key, type(None))):
             raise TypeError(f"customer_key must be an instance of db.Key")
         skel = self.editSkel()
-        if not skel.fromDB(order_key):
+        if not skel.read(order_key):
             raise core_errors.NotFound
         skel = self._order_set_values(
             skel,
@@ -203,7 +203,7 @@ class Order(ShopModuleAbstract, List):
             pass
         skel = self.additional_order_update(skel, **kwargs)
         self.onEdit(skel)
-        skel.toDB()
+        skel.write()
         self.onEdited(skel)
         EVENT_SERVICE.call(Event.ORDER_CHANGED, order_skel=skel, deleted=False)
         return skel
@@ -287,7 +287,7 @@ class Order(ShopModuleAbstract, List):
         if not isinstance(order_key, db.Key):
             raise TypeError(f"order_key must be an instance of db.Key")
         order_skel = self.editSkel()
-        if not order_skel.fromDB(order_key):
+        if not order_skel.read(order_key):
             raise core_errors.NotFound()
         order_skel.refresh()  # TODO: cart.shipping_address relation seems not be updated by the core
         if ClientError.has_failing_error(errors := self.can_checkout(order_skel)):
@@ -306,7 +306,7 @@ class Order(ShopModuleAbstract, List):
             order_skel = HOOK_SERVICE.dispatch(Hook.ORDER_CHECKOUT_START_ADDITION)(order_skel)
         except DispatchError:
             pass
-        order_skel.toDB()
+        order_skel.write()
         self.set_checkout_in_progress(order_skel)
         EVENT_SERVICE.call(Event.ORDER_CHANGED, order_skel=order_skel, deleted=False)
         return JsonResponse({
@@ -343,14 +343,14 @@ class Order(ShopModuleAbstract, List):
         self.shop.cart.freeze_cart(order_skel["cart"]["dest"]["key"], order_skel)
 
         cart_skel = self.shop.cart.viewSkel("node")
-        assert cart_skel.fromDB(order_skel["cart"]["dest"]["key"])
+        assert cart_skel.read(order_skel["cart"]["dest"]["key"])
         order_skel["total"] = cart_skel["total"]
 
         # Clone the address, so in case the user edits the address, existing orders wouldn't be affected by this
         # TODO: Can we do this copy-on-write instead; clone if an address is edited and replace on used order skels?
         ba_skel = self.shop.address.editSkel()
         ba_key = order_skel["billing_address"]["dest"]["key"]
-        assert ba_skel.fromDB(ba_key)
+        assert ba_skel.read(ba_key)
         # Remove the key to clone it #  TODO: why does this not work?
         # ba_skel.dbEntity.key = None
         # ba_skel.accessedValues.pop("key", None)
@@ -364,12 +364,12 @@ class Order(ShopModuleAbstract, List):
             ba_skel[name] = value
         ba_skel.setBoneValue("cloned_from", ba_key)
         # logger.debug(f"{order_skel = }")
-        key = ba_skel.toDB()
+        ba_skel.write()
         # logger.debug(f"{key = } // {ba_skel = }")
         assert ba_skel["key"] != ba_key, \
             f'{ba_skel["key"]} != {ba_key}'
         order_skel.setBoneValue("billing_address", ba_skel["key"])
-        order_skel.toDB()
+        order_skel.write()
         EVENT_SERVICE.call(Event.ORDER_CHANGED, order_skel=order_skel, deleted=False)
 
         return order_skel
@@ -409,7 +409,7 @@ class Order(ShopModuleAbstract, List):
         if not isinstance(order_key, db.Key):
             raise TypeError(f"order_key must be an instance of db.Key")
         order_skel = self.editSkel()
-        if not order_skel.fromDB(order_key):
+        if not order_skel.read(order_key):
             raise core_errors.NotFound()
 
         if ClientError.has_failing_error(errors := self.can_order(order_skel)):

src/viur/shop/modules/shipping.py
@@ -77,7 +77,7 @@ class Shipping(ShopModuleAbstract, List):
         :return: A list of :class:`SkeletonInstance`s for the :class:`ShippingSkel`.
         """
         cart_skel = self.shop.cart.viewSkel("node")
-        if not cart_skel.fromDB(cart_key):
+        if not cart_skel.read(cart_key):
             raise errors.NotFound
 
         all_shipping_configs: list[RefSkel] = []

commit 7da21d5da043bdd05cf1ffb6260e85c6e49a22f5

fix: `basket_key` use existing variable
bug introduced in #102

src/viur/shop/modules/cart.py
@@ -93,7 +93,7 @@ class Cart(ShopModuleAbstract, Tree):
             current.session.get().markChanged()
             # Store basket at the user skel, it will be shared over multiple sessions / devices
             if user := current.user.get():
-                db.RunInTransaction(self._set_basket_txn, user_key=user["key"], basket_key=key)
+                db.RunInTransaction(self._set_basket_txn, user_key=user["key"], basket_key=root_node["key"])
         return self.session["session_cart_key"]
 
     def detach_session_cart(self) -> db.Key: